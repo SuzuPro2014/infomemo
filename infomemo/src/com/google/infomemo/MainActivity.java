4a2ce5e4de07b5137c8c4c74123215bd7903c426
敦子 <@AtsukoOhro>
2014-10-01 23:54:08 +0900

    Wi-Fi情報と位置情報記録用アプリ

---
+++ infomemo/src/com/google/infomemo/MainActivity.java
@@ -0,0 +1,160 @@
+package com.google.infomemo;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		FileList_View();
+
+	}
+
+	public void FileList_View () {
+
+		String[] fileList = this.fileList();
+
+        final ArrayAdapter <String> adapter = new ArrayAdapter <String> (this, android.R.layout.simple_expandable_list_item_1, fileList);
+
+        ListView fList = (ListView) findViewById(R.id.fileList);
+        fList.setAdapter(adapter);
+
+        fList.setOnItemClickListener(
+        		new AdapterView.OnItemClickListener() {
+				public void onItemClick(AdapterView<?> av, View view, int position, long id) {
+					String filename = adapter.getItem(position);
+					showContent(filename);
+				}
+        		});
+
+	}
+
+	public void showContent (final String filename) {
+
+		String str = "", tmp;
+
+		try {
+			 FileInputStream in = openFileInput(filename);
+			 BufferedReader reader = new BufferedReader( new InputStreamReader( in , "UTF-8") );
+
+
+			 while ((tmp = reader.readLine()) != null) {
+				 str = str + tmp + "\n";
+
+			}
+			reader.close();
+		} catch (FileNotFoundException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+
+		AlertDialog.Builder b = new AlertDialog.Builder(this);
+
+		b.setTitle(filename).setMessage(str)
+		.setPositiveButton("Delete",
+				new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						// TODO 自動生成されたメソッド・スタブ
+						check(filename);
+					}
+				})
+		.setNeutralButton("Close",
+				new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						// TODO 自動生成されたメソッド・スタブ
+
+					}
+				}).show();
+
+	}
+
+	public void check (final String filename) {
+
+		AlertDialog.Builder b = new AlertDialog.Builder(this);
+		b.setTitle("Warning!").setMessage("本当に\n" + filename + "\nを削除しますか？")
+		.setPositiveButton("Yes",
+				new DialogInterface.OnClickListener() {
+
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						// TODO 自動生成されたメソッド・スタブ
+						deleteFile(filename);
+						FileList_View();
+
+					}
+				})
+		.setNeutralButton("Cancel",
+				new DialogInterface.OnClickListener() {
+
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						// TODO 自動生成されたメソッド・スタブ
+
+					}
+				})
+		.show();
+
+	}
+
+	public void newData (View view) {
+
+		Intent i = new Intent(this, com.google.infomemo.SaveActivity.class);
+		this.startActivityForResult(i, 1);
+
+	}
+
+	@Override
+	protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+
+		super.onActivityResult(requestCode, resultCode, data);
+
+		if (requestCode == 1 && resultCode == RESULT_OK) {
+			FileList_View();
+		}
+
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
---
+++ infomemo/src/com/google/infomemo/SaveActivity.java
@@ -0,0 +1,216 @@
+package com.google.infomemo;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.text.format.Time;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SaveActivity extends Activity implements LocationListener {
+
+	String wifiData = "◆Wi-Fi情報";
+	String gpsData = "◆位置情報";
+	String date = "";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_save);
+
+		TextView day = (TextView)findViewById(R.id.tv_day);
+
+		Time time = new Time("Asia/Tokyo");
+		time.setToNow();
+		date = time.year + "年" + (time.month + 1) + "月" + time.monthDay + "日" + time.hour + "時" + time.minute + "分" + time.second + "秒";
+		day.setText(date);
+
+
+		getWiFiInfo();
+		getGPSInfo();
+
+
+	}
+
+
+	// データを保存
+	public void dataSave (View view) {
+
+		EditText filename = (EditText)findViewById(R.id.et_filename);
+		EditText etc = (EditText)findViewById(R.id.et_etc);
+
+		String file = filename.getText().toString() +  ".txt";
+		String etc2 = etc.getText().toString();
+
+		String message = "";
+
+		String inputData = "◆記録日時\r\n"+ date + "\r\n\r\n\r\n" + wifiData + gpsData + "◆備考\r\n" + etc2;
+
+		String[] fileList = this.fileList();
+
+		boolean flg = false;
+
+		for (int i = 0; i < fileList.length; i++) {
+			if (file.equals(fileList[i])) {
+				flg = true;
+			}
+		}
+
+		if (flg == false) {
+			try {
+			      FileOutputStream outStream = openFileOutput(file, MODE_PRIVATE);
+			      OutputStreamWriter writer = new OutputStreamWriter(outStream);
+			      writer.write(inputData);
+			      writer.flush();
+			      writer.close();
+
+			      message = "保存しました"
+			      		+ "";
+			    } catch (FileNotFoundException e) {
+			      message = e.getMessage();
+			      e.printStackTrace();
+			    } catch (IOException e) {
+			      message = e.getMessage();
+			      e.printStackTrace();
+			    }
+
+			Toast.makeText(getBaseContext(), message, Toast.LENGTH_LONG).show();
+
+			Intent i = new Intent();
+			setResult(RESULT_OK, i);
+		    finish();
+
+		} else {
+			message = "そのファイル名は既に使われています";
+			Toast.makeText(getBaseContext(), message, Toast.LENGTH_LONG).show();
+		}
+
+
+;
+
+	}
+
+	// 周囲のWi-Fi情報取得とリスト表示
+	public void getWiFiInfo () {
+
+		// Wi-Fiマネージャ取得
+		WifiManager manager = (WifiManager)getSystemService(WIFI_SERVICE);
+
+		// 現在周知しているWi-Fiスポット検索開始
+		manager.startScan();
+
+		List<ScanResult> scanResults = manager.getScanResults();
+		int size = scanResults.size();
+
+		ArrayList<Map<String, String>> wifiInfo = new ArrayList<Map<String, String>>();
+
+		for (int i = 0; i <size; i++) {
+
+			String ssid = scanResults.get(i).SSID;
+			String bssid = scanResults.get(i).BSSID;
+
+			if (ssid != "") {
+				HashMap<String, String> data = new HashMap<String, String>();
+
+				String d = "SSID：" + ssid + "\r\nBSSID：" + bssid + "\r\n";
+
+				wifiData = wifiData + "\r\n" + d;
+
+				data.put("SSID", ssid);
+				data.put("BSSID", bssid);
+				wifiInfo.add(data);
+			}
+		}
+
+		wifiData = wifiData + "\r\n\r\n";
+
+        SimpleAdapter adapter = new SimpleAdapter(this, wifiInfo, android.R.layout.simple_list_item_2, new String[] {"SSID", "BSSID"}, new int[] {android.R.id.text1, android.R.id.text2});
+
+        ListView wifiList = (ListView)findViewById(R.id.wifi);
+        wifiList.setAdapter(adapter);
+
+	}
+
+	// 位置情報取得準備
+	public void getGPSInfo () {
+
+        LocationManager mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+        criteria.setPowerRequirement(Criteria.POWER_MEDIUM);
+
+        String provider = mLocationManager.getBestProvider(criteria, true);
+        mLocationManager.requestLocationUpdates(provider, 0, 0, this);
+
+
+	}
+
+	// 位置情報取得とリスト表示
+	@Override
+	public void onLocationChanged(Location location) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		String [] gps = new String [2];
+
+		gps[0] = "Lat：" + location.getLatitude();
+		gps[1] = "Ing：" + location.getLongitude();
+
+		for (int i = 0; i < gps.length; i++) {
+			gpsData = gpsData + "\r\n" + gps[i];
+		}
+
+		gpsData = gpsData + "\r\n\r\n\r\n";
+
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, gps);
+
+		ListView gpsList = (ListView) findViewById(R.id.fileList);
+		gpsList.setAdapter(adapter);
+
+	}
+
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+
+
+}
